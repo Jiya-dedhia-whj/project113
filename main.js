7166fdfb41edc77ffd79826f3e2f4567c937b632
Jiya-dedhia-whj <94034939+Jiya-dedhia-whj@users.noreply.github.com>
2023-06-05 22:22:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload()
+{
+
+}
+function setup()
+{
+    canvas = createCanvas(600,550);
+    canvas.center();
+}
+function draw()
+{
+
+}
+function take_snapshot()
+{
+    save("mustache.png")
+}
